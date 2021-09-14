42c837f8195c1ae4e0100910f7a9ac24d15eb2b5
Xiaoxia Lang <xxlang@grandstream.cn>
2021-09-14 10:00:38 +0800

    Add version to demos.

--- demo/webhid/requestDevice/main.js
+++ demo/webhid/requestDevice/main.js
@@ -1,24 +1,30 @@
+// Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
 let mainWindow;
 
 async function createMainWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
 			contextIsolation: false
-        }
-    })
+		}
+	})
 
-    mainWindow.webContents.openDevTools()
+	// and load the index.html of the app.
+	mainWindow.loadFile('./index.html')
 
-    mainWindow.loadFile('./index.html')
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
 
-    return mainWindow
+	return mainWindow
 }
 
 app.whenReady().then(() => {
-    createMainWindow()
+	createMainWindow()
 })

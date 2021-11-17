c886b75978d8530307b42f208b2bd693a6f161cd
Xiaoxia Lang <xxlang@grandstream.cn>
2021-11-17 12:03:47 +0800

    Add selfCapturer demo.

---
+++ demo/capturer/selfCapturer/js/render.js
@@ -0,0 +1,73 @@
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+
+async function startCamera() {
+  const constraints = {
+    audio: false,
+    video: {
+      cursor: "always"
+    }
+  };
+
+  // Create a Stream
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  // Preview the source in a video element
+  const videoElement = document.getElementById('cameraVideo');
+  videoElement.srcObject = stream;
+  videoElement.play();
+}
+
+async function stopCamera() {
+  const videoElement = document.getElementById('cameraVideo');
+  let tracks = videoElement.srcObject.getTracks();
+
+  tracks.forEach(track => track.stop());
+  videoElement.srcObject = null;
+}
+
+async function startScreen() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name + ' [' + source.id + ']',
+        click: () => selectSource(source)
+      };
+    })
+  );
+
+
+  videoOptionsMenu.popup();
+}
+
+async function selectSource(source) {
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
+    }
+  };
+
+  // Create a Stream
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  // Preview the source in a video element
+  const videoElement = document.getElementById('screenVideo');
+  videoElement.srcObject = stream;
+  videoElement.play();
+}
+
+async function stopScreen() {
+  const videoElement = document.getElementById('screenVideo');
+  let tracks = videoElement.srcObject.getTracks();
+
+  tracks.forEach(track => track.stop());
+  videoElement.srcObject = null;
+}

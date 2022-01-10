2baa454864115a19539015c94292177f4cb22df9
Xiaoxia Lang <xxlang@grandstream.cn>
2022-01-10 17:05:07 +0800

    Add zip debug script.

---
+++ dist/zip_debug.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd `e show root`
+cd src
+electron/script/copy-debug-symbols.py --target-cpu="x64" --out-dir=out/`e show out`/debug --compress -d out/`e show out`
+electron/script/strip-binaries.py --target-cpu="x64" -d out/`e show out`
+electron/script/add-debug-link.py --target-cpu="x64" --debug-dir=out/`e show out`/debug -d out/`e show out`
+cd out/`e show out`
+zip debug.zip debug/*.debug

9b8b28b8cb5c04b774bab3004718707eafdda66f
Xiaoxia Lang <xxlang@grandstream.cn>
2022-01-11 15:13:26 +0800

    Modify zip_pdb script.

--- dist/zip_pdb.bat
+++ dist/zip_pdb.bat
@@ -1,3 +1,3 @@
 for /F %%i in ('e show outdir') do set "electron_outdir=%%i"
 cd %electron_outdir%
-start winrar a -afzip pdb.zip *.pdb
+start winrar a -afzip electron-v13.6.3-win32-x64-pdb.zip *.pdb

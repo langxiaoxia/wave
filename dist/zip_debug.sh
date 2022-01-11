207649946c850dad26fc056c1f9c5a0e858887c9
Xiaoxia Lang <xxlang@grandstream.cn>
2022-01-11 15:07:59 +0800

    Modify zip_debug script

--- dist/zip_debug.sh
+++ dist/zip_debug.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 cd `e show root`
 cd src
 electron/script/copy-debug-symbols.py --target-cpu="x64" --out-dir=out/`e show out`/debug --compress -d out/`e show out`
 electron/script/strip-binaries.py --target-cpu="x64" -d out/`e show out`
 electron/script/add-debug-link.py --target-cpu="x64" --debug-dir=out/`e show out`/debug -d out/`e show out`
 cd out/`e show out`
-zip debug.zip debug/*.debug
+zip electron-v13.6.3-linux-x64-debug.zip debug/*.debug

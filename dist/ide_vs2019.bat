ea50bd40883095e4a4a912351b4da39044b37b66
Xiaoxia Lang <xxlang@grandstream.cn>
2022-02-15 15:41:15 +0800

    Modify ide script on Windows.

--- dist/ide_vs2019.bat
+++ dist/ide_vs2019.bat
@@ -2,7 +2,14 @@
 
 setlocal
 
+call e use dev_testing
 for /F %%i in ('e show root') do set "root_dir=%%i"
 cd %root_dir%\src
 set CHROMIUM_BUILDTOOLS_PATH=%cd%\buildtools
-gn gen out/x64 --args="import(\"//electron/build/args/release.gn\") target_cpu=\"x64\"" --ide=vs2019
+call gn gen out/x64_testing --args="import(\"//electron/build/args/testing.gn\") target_cpu=\"x64\"" --ide=vs2019 --sln=x64_testing_all
+
+call e use dev_release
+for /F %%i in ('e show root') do set "root_dir=%%i"
+cd %root_dir%\src
+set CHROMIUM_BUILDTOOLS_PATH=%cd%\buildtools
+call gn gen out/x64_release --args="import(\"//electron/build/args/release.gn\") target_cpu=\"x64\"" --ide=vs2019 --sln=x64_release_all

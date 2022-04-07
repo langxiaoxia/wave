dbb7f819d8a9c416964bbf31bcf75297fa419ace
Xiaoxia Lang <xxlang@grandstream.cn>
2022-04-07 09:43:39 +0800

    Add Chromium build scripts.

---
+++ configs/win32/proxy_on.cmd
@@ -0,0 +1,4 @@
+@echo off
+set NO_AUTH_BOTO_CONFIG=%USERPROFILE%\https_proxy.boto
+set http_proxy=http://127.0.0.1:8118
+set https_proxy=http://127.0.0.1:8118
---
+++ configs/win32/set_chrome_env.cmd
@@ -0,0 +1,4 @@
+@echo off
+set DEPOT_TOOLS_UPDATE=0
+set GIT_CACHE_PATH=%USERPROFILE%\.git_cache
+set PATH=%USERPROFILE%\depot_tools;%PATH%

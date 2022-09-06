61f137041520327afadd4c870d30bc28e4715780
Xiaoxia Lang <xxlang@grandstream.cn>
2022-09-06 08:33:37 +0800

    chore: ide for vs2019.

--- dist/ide_vs2019.bat
+++ dist/ide_vs2019.bat
@@ -2,14 +2,10 @@
 
 setlocal
 
-call e use dev_testing
 for /F %%i in ('e show root') do set "root_dir=%%i"
 cd %root_dir%\src
 set CHROMIUM_BUILDTOOLS_PATH=%cd%\buildtools
-call gn gen out/x64_testing --args="import(\"//electron/build/args/testing.gn\") target_cpu=\"x64\"" --ide=vs2019 --sln=x64_testing_all
 
-call e use dev_release
-for /F %%i in ('e show root') do set "root_dir=%%i"
-cd %root_dir%\src
-set CHROMIUM_BUILDTOOLS_PATH=%cd%\buildtools
-call gn gen out/x64_release --args="import(\"//electron/build/args/release.gn\") target_cpu=\"x64\"" --ide=vs2019 --sln=x64_release_all
+for /F %%i in ('e show current') do set "config_name=%%i"
+for /F %%i in ('e show out') do set "out_name=%%i"
+call gn gen out/%out_name% --args="import(\"//electron/build/args/release.gn\") target_cpu=\"x64\"" --ide=vs2019 --sln=electron_%config_name%

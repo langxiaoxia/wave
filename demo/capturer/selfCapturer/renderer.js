cea076804bce8ec4b72d5a7ba16af37c56146e5b
Xiaoxia Lang <xxlang@grandstream.cn>
2022-12-24 17:04:11 +0800

    feat: fix self capturer.

--- demo/capturer/selfCapturer/renderer.js
+++ demo/capturer/selfCapturer/renderer.js
@@ -1,73 +1,26 @@
 const { desktopCapturer, remote } = require('electron');
 const { Menu } = remote;
 
-const startCameraBtn = document.querySelector('#startCamera');
-const stopCameraBtn = document.querySelector('#stopCamera');
+const listSourceBtn = document.querySelector('#listSource');
 const startScreenBtn = document.querySelector('#startScreen');
 const stopScreenBtn = document.querySelector('#stopScreen');
 
-const cameraVideoElement = document.getElementById('cameraVideo');
 const screenVideoElement = document.getElementById('screenVideo');
 
-function onCameraInactive() {
-  console.log("onCameraInactive");
-  startCameraBtn.disabled = false;
-  stopCameraBtn.disabled = true;
-  cameraVideoElement.srcObject = null;
-}
+async function listSource() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
 
-async function startCamera() {
-  const constraints_without_audio = {
-    audio: false,
-    video: {
-      cursor: "always",
-      frameRate: 30
-    }
-  };
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name + ' [' + source.id + ']'
+      };
+    })
+  );
 
-  const constraints_with_audio = {
-    audio: true,
-    video: {
-      cursor: "always",
-      frameRate: 30
-    }
-  };
-
-  let stream = await navigator.mediaDevices.getUserMedia(constraints_without_audio);
-  if (stream && stream.active) {
-    stream.oninactive = onCameraInactive;
-
-    let tracks = stream.getVideoTracks();
-    tracks[0].onended = function() {
-      console.log("video track onended");
-    }
-
-    tracks[0].onmute = function(e) {
-      console.log("video track onmute", e);
-    }
-
-    tracks[0].onunmute = function(e) {
-      console.log("video track onunmute", e);
-    }
-
-    cameraVideoElement.srcObject = stream;
-    cameraVideoElement.play();
-
-    startCameraBtn.disabled = true;
-    stopCameraBtn.disabled = false;
-  }
-}
-
-async function stopCamera() {
-  if (cameraVideoElement.srcObject) {
-    let tracks = cameraVideoElement.srcObject.getTracks();
-    if (tracks) {
-      tracks.forEach(track => {
-      	console.log("stop track", track.kind, ":", track.label);
-      	track.stop();
-      });
-    }
-  }
+  videoOptionsMenu.popup();
 }
 
 async function startScreen() {
@@ -128,15 +81,15 @@ async function selectSource(source) {
     stream.oninactive = onScreenInactive;
 
     let tracks = stream.getVideoTracks();
-    tracks[0].onended = function() {
+    tracks[0].onended = function () {
       console.log("video track onended");
     }
 
-    tracks[0].onmute = function(e) {
+    tracks[0].onmute = function (e) {
       console.log("video track onmute", e);
     }
 
-    tracks[0].onunmute = function(e) {
+    tracks[0].onunmute = function (e) {
       console.log("video track onunmute", e);
     }
 
@@ -154,8 +107,8 @@ async function stopScreen() {
     let tracks = screenVideoElement.srcObject.getTracks();
     if (tracks) {
       tracks.forEach(track => {
-      	console.log("stop track", track.kind, ":", track.label);
-      	track.stop();
+        console.log("stop track", track.kind, ":", track.label);
+        track.stop();
       });
     }
   }

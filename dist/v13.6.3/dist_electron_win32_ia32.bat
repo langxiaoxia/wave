5a25f962fb2ab4e378e506d6a003c4b751361f58
Xiaoxia Lang <xxlang@grandstream.cn>
2022-12-24 17:00:45 +0800

    chore: v13.6.3 dist scripts.

---
+++ dist/v13.6.3/dist_electron_win32_ia32.bat
@@ -0,0 +1,33 @@
+@echo off
+
+setlocal
+
+set NO_AUTH_BOTO_CONFIG=%USERPROFILE%\https_proxy.boto
+set http_proxy=http://127.0.0.1:8118
+set https_proxy=http://127.0.0.1:8118
+
+set build_tag=v13.6.3
+
+call e use v13_x86
+
+for /F %%i in ('e show root') do set "root_dir=%%i"
+for /F %%i in ('e show out') do set "out_name=%%i"
+set out_dir=%root_dir%\src\out\%out_name%
+
+cd %root_dir%\src\electron
+call git pull
+call e sync
+
+copy /b %root_dir%\src\electron\ELECTRON_VERSION+,,
+call e build
+
+del %out_dir%\dist.zip
+call e build electron:dist
+del %out_dir%\electron-%build_tag%-win32-ia32.zip
+ren %out_dir%\dist.zip electron-%build_tag%-win32-ia32.zip
+
+cd %root_dir%\src
+del %out_dir%\pdb.zip
+call python electron\script\zip-symbols.py -b %out_dir%
+del %out_dir%\electron-%build_tag%-win32-ia32-pdb.zip
+ren %out_dir%\pdb.zip electron-%build_tag%-win32-ia32-pdb.zip
